2875f18c9c2249a8dec77d29d8bd8740b84fea27
Gelsey Torres <gelsey.torres@gmail.com>
2021-01-20 11:05:06 -0500

    changes column to 'type'

--- db/migrate/20210120155519_create_companies.rb
+++ db/migrate/20210120155519_create_companies.rb
@@ -2,7 +2,7 @@ class CreateCompanies < ActiveRecord::Migration[6.0]
   def change
     create_table :companies do |t|
       t.string :name
-      t.string :type_of_company
+      t.string :type
       t.string :industry
       t.string :product
       t.text :comments
