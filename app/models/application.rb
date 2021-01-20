3fc01c9800d3241271a32081d4b2426c58218de0
Gelsey Torres <gelsey.torres@gmail.com>
2021-01-20 12:28:42 -0500

    Adds validations for Application

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,2 +1,9 @@
 class Application < ApplicationRecord
+  belongs_to :company
+  has_many :interviews
+  has_many :questions
+  has_one :process
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
