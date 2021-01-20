d9b5859d60a8b80c0f2d07f41883ecbcd262c7ad
Gelsey Torres <gelsey.torres@gmail.com>
2021-01-20 12:28:59 -0500

    Adds validations for Company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,8 @@
 class Company < ApplicationRecord
+  has_many :employees, depend: :destroy
+  has_many :applications, depend: :destroy
+
+  validates :name, presence: true
+  validates :type, presence: true
+  validates :industry, presence: true
 end
