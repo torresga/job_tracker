259c77dd0999662856c57055c11e4170b93e8f1e
Gelsey Torres <gelsey.torres@gmail.com>
2021-02-04 14:55:42 -0500

    Changes navigation to use react bootstrap nav components

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { render } from "react-dom";
 
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Applications from "../components/Applications";
 import Interviews from "../components/Interviews";
 import Companies from "../components/Companies";
@@ -10,33 +14,42 @@ import NewCompany from "../components/NewCompany";
 
 function App() {
   return (
-    <div>
-      <nav>
-        <Link to="/">Companies</Link>
-        <Link to="/applications">Applications</Link>
-        <Link to="/interviews">Interviews</Link>
-        <Link to="/companies/new">Add a company</Link>
-      </nav>
+    <>
+      <Nav activeKey="/home" className="top-nav">
+        <Nav.Item>
+          <Nav.Link as={Link} to="/">Companies</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/applications">Applications</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/interviews">Interviews</Nav.Link>
+        </Nav.Item>
+      </Nav>
 
-      <Switch>
-        <Route path="/applications" component={Applications} />
-        <Route path="/interviews" component={Interviews} />
-        <Route path="/companies/new">
-          <NewCompany />
-        </Route>
-        <Route path="/">
-          <Companies />
-        </Route>
-      </Switch>
-    </div>
+      <Container className="companies mt-3 p-3">
+        <Switch>
+          <Route path="/applications" component={Applications} />
+          <Route path="/interviews" component={Interviews} />
+          <Route path="/companies/new">
+            <NewCompany />
+          </Route>
+          <Route path="/">
+            <Companies />
+          </Route>
+        </Switch>
+      </Container>
+    </>
   );
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   render (
+    <Container fluid={true}>
     <Router>
       <App />
-    </Router>,
+    </Router>
+    </Container>,
     document.body.appendChild(document.createElement("div"))
   );
 });
