4c14a9fe08bcb6e631532caf00d274d1f1cfe9e4
Gelsey Torres <gelsey.torres@gmail.com>
2021-02-11 16:47:47 -0500

    Adds the update action to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :companies, only: [:index, :create, :show, :destroy]
+    resources :companies, only: [:index, :create, :show, :edit, :update, :destroy]
   end
 
   root 'homepage#index'
